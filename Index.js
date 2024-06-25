977f3c81704283890a4f668319bc3a3b22067929
Khayti1102 <115869656+Khayti1102@users.noreply.github.com>
2024-06-25 22:22:12 +0530

    Initial commit

---
+++ Index.js
@@ -0,0 +1,11 @@
+
+function scrollToAboutMe() {
+    var aboutMeSection = document.getElementById('ThirdCard');
+    aboutMeSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToMyProjects(){
+    var MyProjectSection=document.getElementById('Project-2');
+    MyProjectSection.scrollIntoView({ behavior: 'smooth' });
+}
+
